5e143c33ea2509c2e9f40e5e9ea79a058d21e22f
Alan Daniels <alan.daniels10@gmail.com>
2024-11-26 14:51:10 +1100

    Update keymap.c

--- keyboards/lily58/keymaps/Alan-Daniels/keymap.c
+++ keyboards/lily58/keymaps/Alan-Daniels/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* COLMAK-DH
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  ESC |      |      |      |      |      |                    |      |      |      |      |      |  `   |
+ * |  ESC |   1  |   2  |   3  |   4  |  5   |                    |   6  |   7  |   8  |   9  |   0  |  `   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  TAB |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   ;  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 **/
 
  [_COLMAK] = LAYOUT( \
-  KC_ESC,   _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, KC_GRV,  \
+  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,  \
   KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS, \
   KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_D,    KC_V, KC_LBRC,  KC_RBRC,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_CAPS, \
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 **/
 
  [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_F19, KC_F21,  KC_F22,  KC_F23,  KC_F24,                   KC_F18,  KC_F17,  KC_F16,  KC_F15,  KC_F14,  KC_GRV, \
+  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,  \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_CAPS, \
